0595640d4153b51f417cb7a8160beea045eddf24
CarlosRial-23 <133531066+CarlosRial-23@users.noreply.github.com>
2023-06-16 00:10:44 -0300

    primer commit des STS

---
+++ src/main/java/com/grupo4/grupo4OO22023/Grupo4Oo22023Application.java
@@ -0,0 +1,13 @@
+package com.grupo4.grupo4OO22023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo4Oo22023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo4Oo22023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo4/grupo4OO22023/Grupo4Oo22023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4.grupo4OO22023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo4Oo22023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
